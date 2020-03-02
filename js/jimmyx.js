4f1b6ca7b52c89db5e1ce5ad907b2e22e78ae698
hamraniali <ali.hamrani80@gmail.com>
2020-03-02 16:39:26 +0330

    0.5 of project completed

---
+++ js/jimmyx.js
@@ -0,0 +1,27 @@
+$(function () {
+    AOS.init();
+    $('#my_skills').click(function () {
+        $('.chart').easyPieChart({
+            barColor: '#d41b3f',
+            trackColor: '#251c1a',
+            scaleColor: '#251c1a',
+            scaleLength: 0,
+            lineCap: 'butt',
+            lineWidth: 12,
+            trackWidth: undefined,
+            size: 110,
+            rotate: 0, // in degrees
+            animate: {
+                duration: 2000,
+                enabled: true
+            },
+            easing: function (x, t, b, c, d) { // easing function
+                t = t / (d / 2);
+                if (t < 1) {
+                    return c / 2 * t * t + b;
+                }
+                return -c / 2 * ((--t) * (t - 2) - 1) + b;
+            }
+        });
+    })
+});
